15d1bc68e0b95d7b4a897da3ddd98317247d30e2
Nilson Roman <nilsonoroman@gmail.com>
2024-02-15 20:32:17 -0300

    feat: add webserver

---
+++ src/infrastructure/api/express.ts
@@ -0,0 +1,28 @@
+import express, { Express } from "express";
+import { Sequelize } from "sequelize-typescript";
+import ClientModel from "../../modules/client-adm/repository/client.model";
+import ProductModel from "../../modules/store-catalog/repository/product.model";
+import ProductAdmModel from "../../modules/product-adm/repository/product-adm.model";
+import TransactionModel from "../../modules/payment/repository/transaction.model";
+import InvoiceModel from "../../modules/invoice/repository/invoice.model";
+import InvoiceItemModel from "../../modules/invoice/repository/invoice-item.model";
+
+export const app: Express = express();
+app.use(express.json());
+
+export let sequelize: Sequelize;
+
+async function setupDb() {
+    sequelize = new Sequelize({
+        dialect: 'sqlite',
+        storage: ':memory:',
+        logging: false,
+        sync: { force: true }
+    });
+    await sequelize.addModels([ClientModel, ProductAdmModel, ProductModel, 
+        TransactionModel, InvoiceModel, InvoiceItemModel]);
+
+    await sequelize.sync();
+}
+
+setupDb();
