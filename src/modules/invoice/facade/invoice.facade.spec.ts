8da55fe85179d3d3235bdd305a30832a82a54762
Nilson Roman <nilsonoroman@gmail.com>
2024-02-18 20:05:14 -0300

    fix: import  order for invoice facade factory

--- src/modules/invoice/facade/invoice.facade.spec.ts
+++ src/modules/invoice/facade/invoice.facade.spec.ts
@@ -1,7 +1,7 @@
 import { Sequelize } from "sequelize-typescript"
+import InvoiceFacadeFactory from "../factory/invoice.facade.factory"
 import InvoiceModel from "../repository/invoice.model"
 import InvoiceItemModel from "../repository/invoice-item.model"
-import InvoiceFacadeFactory from "../factory/invoice.facade.factory"
 
 describe("Invoice Facade unit test", () => {
 
