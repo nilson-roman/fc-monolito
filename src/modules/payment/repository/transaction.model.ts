89dd37ab6a3c6b88d9df88446beb863989d7800e
Nilson Roman <nilsonoroman@gmail.com>
2024-02-21 19:56:34 -0300

    feat: add transaction migration file

--- src/modules/payment/repository/transaction.model.ts
+++ src/modules/payment/repository/transaction.model.ts
@@ -9,7 +9,7 @@ export default class TransactionModel extends Model {
   @Column({ allowNull: false })
   declare id: string;
 
-  @Column({ allowNull: false, field: "order_id" })
+  @Column({ allowNull: false })
   declare orderId: string;
 
   @Column({ allowNull: false })
@@ -18,9 +18,9 @@ export default class TransactionModel extends Model {
   @Column({ allowNull: false })
   declare status: string;
 
-  @Column({ allowNull: false, field: "created_at" })
+  @Column({ allowNull: false })
   declare createdAt: Date;
 
-  @Column({ allowNull: false, field: "updated_at" })
+  @Column({ allowNull: false })
   declare updatedAt: Date;
 }
