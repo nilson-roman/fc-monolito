cdb716fa31aa8ccb57c72c0917063592710a4fcf
Nilson Roman <nilsonoroman@gmail.com>
2024-02-21 21:29:08 -0300

    feat: add order migration file

---
+++ src/configs/sequelize/migrations/2024.02.22T00.15.40.order-migration.ts
@@ -0,0 +1,33 @@
+import { DataTypes, Sequelize } from 'sequelize';
+import type { MigrationFn } from 'umzug';
+
+export const up: MigrationFn<Sequelize> = async ({ context: sequelize }) => {
+    await sequelize.getQueryInterface().createTable('orders', {
+        id: {
+            type: DataTypes.STRING(255),
+            primaryKey: true,
+            allowNull: false
+        },
+        status: {
+            type: DataTypes.STRING(255),
+            allowNull: false
+        },
+        invoiceId: {
+            type: DataTypes.STRING(255),
+            allowNull: true
+        },
+        client: {
+            type: DataTypes.JSON,
+            allowNull: false
+        },
+        products: {
+            type: DataTypes.JSONB,
+            allowNull: false,
+            defaultValue: []
+        }
+    });
+};
+
+export const down: MigrationFn<Sequelize> = async ({ context: sequelize }) => {
+    await sequelize.getQueryInterface().dropTable('orders');
+};
